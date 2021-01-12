1e6fd758f5ed6bd8d265048c9adbddb4d362c6b1
Nika <64929516+nikule4ka@users.noreply.github.com>
2021-01-13 00:24:05 +0200

    adds files

---
+++ src/js/module1/task-34.js
@@ -0,0 +1,12 @@
+// Функция normalizeInput(input) принимает строку (параметр input) и возвращает такую же строку, но в нижнем регистре.
+//Присвой переменной normalizedInput выражение создания строки
+//в нижнем регистре из параметра input.
+
+// Вызов функции normalizeInput('Привет мир') возвращает 'привет мир'.
+// Вызов функции normalizeInput('Это НЕ СпаМ') возвращает 'это не спам'.
+// Вызов функции normalizeInput('Большие СКИДКИ') возвращает 'большие скидки'.
+
+function normalizeInput(input) {
+  const normalizedInput = input.toLowerCase(); // Дополни эту строку
+  return normalizedInput;
+}
