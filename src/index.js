f13c54cfc1993e0b6c1a4682d5a2caf39f75a795
Nika <64929516+nikule4ka@users.noreply.github.com>
2021-01-13 00:53:44 +0200

    task36

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './styles.css';
+
+function checkForSpam(message) {
+  let result;
+  // Пиши код ниже этой строки
+  const saleSpam = 'spam' + 'sale';
+  result = message.includes(saleSpam.toLowerCase().toUpperCase());
+  // Пиши код выше этой строки
+  return result;
+}
+
+console.log(checkForSpam('Amazing SalE, only tonight!'));
---
+++ src/js/module1/task-36.js
@@ -0,0 +1,9 @@
+function checkForSpam(message) {
+  let result;
+  // Пиши код ниже этой строки
+  result = message.includes().toUpperCase().to;
+  // Пиши код выше этой строки
+  return result;
+}
+
+console.log(checkForSpam('Amazing SalE, only tonight!'));
